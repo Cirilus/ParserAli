a587f7286406616b15b98a27b98dfe3492d67e8c
Kirill <shkolenko02-03-2004@mail.ru>
2023-03-08 12:37:24 +0300

    Return user info

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserView
+
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserView.as_view({"get":"list"}), name="user_info"),
 ]
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import CustomUser
+
+@extend_schema(summary="Return the info about register user")
+class UserView(GenericViewSet, ListModelMixin):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+
+        return [self.request.user]
